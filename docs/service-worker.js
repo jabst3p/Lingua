035ce63210a608b22ab800eaec2fa79f4dbe5222
jabst3p <jabst3p@gmail.com>
2026-04-27 04:49:47 +0000

    Fix paths for GitHub Pages deployment

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -4,10 +4,16 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE).then(cache =>
       cache.addAll([
-        "index.html",
-        "styles.css",
-        "app.js",
-        "manifest.webmanifest"
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./manifest.webmanifest",
+        "./data/wiktionary.json",
+        "./data/webster1913.json",
+        "./data/etymonline.json",
+        "./data/idioms.json",
+        "./data/collocations.json",
+        "./data/examples.json"
       ])
     )
   );
